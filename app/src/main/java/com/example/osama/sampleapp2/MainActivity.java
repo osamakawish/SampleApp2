3d70b3e2a44d3ed80bc990b8f9f0e04be73588e5
Osama Kawish <osamakawish@gmail.com>
2017-11-04 09:03:07 -0400

    Started sample application for Android.

---
+++ app/src/main/java/com/example/osama/sampleapp2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.osama.sampleapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "You clicked!",
+                        Toast.LENGTH_LONG).show();
+                
+            }
+        });
+    }
+}
